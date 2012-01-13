b247aa53ad12152e1a62e1c85727406288e3e55c
interkarma@dfworkshop.net <interkarma@dfworkshop.net@003ca66a-1c7e-5d5a-c246-7fb3df14bb5e>
2012-01-13 11:51:43 +0000

    Checking in missing files.

---
+++ Games/RoHD Playground/RoHD Playground/Components/PulsingPointLight.cs
@@ -0,0 +1,40 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using DeepEngine.Core;
+using DeepEngine.Primitives;
+using DeepEngine.Components;
+using DeepEngine.World;
+#endregion
+
+namespace RoHD_Playground.Components
+{
+
+    /// <summary>
+    /// A point light that changes radius based on a sine and some randomness.
+    /// </summary>
+    public class PulsingPointLight : LightComponent
+    {
+
+        #region Fields
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="core"></param>
+        public PulsingPointLight(DeepCore core)
+            : base(core, Vector3.Zero, 1f, Color.White, 1f)
+        {
+        }
+
+        #endregion
+
+    }
+
+}
