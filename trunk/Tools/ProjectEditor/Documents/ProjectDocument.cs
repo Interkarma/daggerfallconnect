6965ad2c9a579c2a21c16b849f30feebd64f4a95
interkarma@dfworkshop.net <interkarma@dfworkshop.net@003ca66a-1c7e-5d5a-c246-7fb3df14bb5e>
2012-04-20 03:15:26 +0000

    Started adding project document management to editor.

---
+++ trunk/Tools/ProjectEditor/Documents/ProjectDocument.cs
@@ -0,0 +1,37 @@
+﻿// Project:         Deep Engine
+// Description:     3D game engine for Ruins of Hill Deep and Daggerfall Workshop projects.
+// Copyright:       Copyright (C) 2012 Gavin Clayton
+// License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
+// Web Site:        http://www.dfworkshop.net
+// Contact:         Gavin Clayton (interkarma@dfworkshop.net)
+// Project Page:    http://code.google.com/p/daggerfallconnect/
+
+#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.ComponentModel;
+using System.Reflection;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using DeepEngine.Core;
+using DeepEngine.Components;
+using DeepEngine.World;
+using SceneEditor.Proxies;
+using SceneEditor.UserControls;
+#endregion
+
+namespace SceneEditor.Documents
+{
+
+    /// <summary>
+    /// Wraps a project into a document for the editor.
+    /// </summary>
+    class ProjectDocument
+    {
+    }
+
+}
