bc3fd6770c037237920419b37e91bcf97cf3404e
interkarma@dfworkshop.net <interkarma@dfworkshop.net@003ca66a-1c7e-5d5a-c246-7fb3df14bb5e>
2012-05-09 08:46:14 +0000

    Started work on zip-based project file format.

---
+++ trunk/Tools/Deep Engine/Deep Engine/Utility/XmlHelper.cs
@@ -0,0 +1,177 @@
+﻿// Project:         Deep Engine
+// Description:     3D game engine for Ruins of Hill Deep and Daggerfall Workshop projects.
+// Copyright:       Copyright (C) 2012 Gavin Clayton
+// License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
+// Web Site:        http://www.dfworkshop.net
+// Contact:         Gavin Clayton (interkarma@dfworkshop.net)
+// Project Page:    http://code.google.com/p/daggerfallconnect/
+
+#region Using Statements
+using System;
+using System.Xml;
+using System.Text;
+using System.IO;
+#endregion
+
+namespace DeepEngine.Utility
+{
+
+    /// <summary>
+    /// Helper class to create XML documents and append elements.
+    /// </summary>
+    public class XmlHelper
+    {
+
+        #region Fields
+
+        private const string extension = ".xml";
+        private const string version = "1.0";
+        private const string encoding = "UTF-8";
+        private const string indent = "    ";
+
+        #endregion
+
+        #region Static Properties
+
+        /// <summary>
+        /// Gets .xml extension.
+        /// </summary>
+        static public string Extension
+        {
+            get { return extension; }
+        }
+
+        /// <summary>
+        /// Gets XML version.
+        /// </summary>
+        static public string Version
+        {
+            get { return version; }
+        }
+
+        /// <summary>
+        /// Gets encoding.
+        /// </summary>
+        static public string Encoding
+        {
+            get { return encoding; }
+        }
+
+        /// <summary>
+        /// Gets XMLWriterSettings.
+        /// </summary>
+        static public XmlWriterSettings Settings
+        {
+            get
+            {
+                XmlWriterSettings settings = new XmlWriterSettings();
+                settings.Indent = true;
+                settings.IndentChars = indent;
+                return settings;
+            }
+        }
+
+        #endregion
+
+        #region Static Methods
+
+        /// <summary>
+        /// Creates XML document.
+        /// </summary>
+        /// <param name="schemaName">Name of schema.</param>
+        /// <returns>XML document.</returns>
+        static public XmlDocument CreateXmlDocument(string schemaName)
+        {
+            // Create XML document, set schema, and get declaration
+            XmlDocument document = new XmlDocument();
+            XmlDeclaration declaration = document.CreateXmlDeclaration(version, encoding, string.Empty);
+
+            // Create writer and start XML file
+            MemoryStream stream = new MemoryStream();
+            XmlWriter writer = XmlWriter.Create(stream, Settings);
+            writer.WriteProcessingInstruction(declaration.Name, declaration.Value);
+            writer.WriteStartElement(schemaName);
+            writer.Close();
+
+            // Load document from stream
+            stream.Seek(0, SeekOrigin.Begin);
+            document.Load(stream);
+
+            return document;
+        }
+
+        /// <summary>
+        /// Creates XML document to a file.
+        /// </summary>
+        /// <param name="schemaName">Name of schema.</param>
+        /// <param name="filename">Path to output file.</param>
+        /// <returns>XML document.</returns>
+        static public XmlDocument CreateXmlDocument(string schemaName, string filename)
+        {
+            // Ensure ".xml" is appended to document
+            if (!filename.EndsWith(extension))
+                filename += extension;
+
+            // Create XML document, set schema, and get declaration
+            XmlDocument document = new XmlDocument();
+            XmlDeclaration declaration = document.CreateXmlDeclaration(version, encoding, string.Empty);
+
+            // Create writer and start XML file
+            XmlWriter writer = XmlWriter.Create(filename, Settings);
+            writer.WriteProcessingInstruction(declaration.Name, declaration.Value);
+            writer.WriteStartElement(schemaName);
+            writer.Close();
+
+            // Load created document
+            document.Load(filename);
+
+            return document;
+        }
+
+        /// <summary>
+        /// Appends an element to XML node.
+        /// </summary>
+        /// <param name="document">Document object.</param>
+        /// <param name="parentNode">Parent node for append.</param>
+        /// <param name="elementName">Name of new element.</param>
+        /// <param name="elementInnerText">Inner text of new element.</param>
+        /// <returns></returns>
+        static public XmlElement AppendElement(XmlDocument document, XmlNode parentNode, string elementName, string elementInnerText)
+        {
+            // If ParentNode isn't set then get root node
+            if (null == parentNode)
+                parentNode = document.DocumentElement;
+
+            // If ParentNode still isn't set then exit
+            if (null == parentNode)
+                return null;
+
+            // Create child element and return new node
+            XmlElement element = document.CreateElement(elementName);
+            element.InnerText = elementInnerText;
+            XmlNode node = parentNode.AppendChild(element);
+            return element;
+        }
+
+        /// <summary>
+        /// Saves an XML document to a memory stream.
+        /// </summary>
+        /// <param name="document">Input document.</param>
+        /// <returns>Memory stream.</returns>
+        static public MemoryStream ToMemoryStream(XmlDocument document)
+        {
+            // Save to stream
+            MemoryStream stream = new MemoryStream();
+            document.Save(stream);
+
+            // Seek back to start of stream
+            stream.Seek(0, SeekOrigin.Begin);
+
+            return stream;
+        }
+
+        #endregion
+
+    }
+
+}
--- trunk/Tools/ProjectEditor/Documents/ProjectDocument.cs
+++ trunk/Tools/ProjectEditor/Documents/ProjectDocument.cs
@@ -17,9 +17,11 @@ using System.ComponentModel;
 using System.Reflection;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Ionic.Zip;
 using DeepEngine.Core;
 using DeepEngine.Components;
 using DeepEngine.World;
+using DeepEngine.Utility;
 using ProjectEditor.Proxies;
 using ProjectEditor.UserControls;
 #endregion
@@ -28,10 +30,177 @@ namespace ProjectEditor.Documents
 {
 
     /// <summary>
-    /// Wraps a project into a document for the editor.
+    /// Interface to a project file. Handles adding, deleting, moving, and renaming files and folders.
+    ///  Manages a single zipped archive containing all files.
     /// </summary>
     class ProjectDocument
     {
+
+        #region Fields
+
+        // Constants
+        const string projectExtension = ".zip";
+        const string projectDescription = "Deep Engine Project File";
+        const string projectSchema = "ProjectDirectory";
+        const string projectDirectoryFileName = "_ProjectDirectory.xml";
+        const string projectElement = "Project";
+        const string projectNameAttribute = "Name";
+
+        // Project xml document
+        ZipFile projectFile;
+        XmlDocument projectDocument;
+
+        // State
+        bool projectOpen = false;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets extension of a project file.
+        /// </summary>
+        static public string Extension
+        {
+            get { return projectExtension; }
+        }
+
+        /// <summary>
+        /// Gets description of a project file.
+        /// </summary>
+        static public string Description
+        {
+            get { return projectDescription; }
+        }
+
+        /// <summary>
+        /// Gets filter for file dialogs.
+        /// </summary>
+        static public string Filter
+        {
+            get { return string.Format("{0} (*{1})|*{1}", projectDescription, projectExtension); }
+        }
+
+        /// <summary>
+        /// Gets flag stating if project is open or closed.
+        /// </summary>
+        public bool IsProjectOpen
+        {
+            get { return projectOpen; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public ProjectDocument()
+        {
+        }
+       
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Creates empty project file at specified path.
+        /// </summary>
+        /// <param name="filename">Full path to project file.</param>
+        public void CreateProjectFile(string filename)
+        {
+            // Create project zip file
+            projectFile = new ZipFile(filename);
+
+            // Create project file inside of zip file
+            string name = Path.GetFileNameWithoutExtension(filename);
+            CreateProjectDirectoryFile(name, projectFile);
+
+            // Save file
+            projectFile.Save();
+
+            // Flag as open
+            projectOpen = true;
+        }
+
+        /// <summary>
+        /// Opens existing project file at specified path.
+        /// </summary>
+        /// <param name="filename">Full path to project file.</param>
+        public void OpenProjectFile(string filename)
+        {
+            // Open project zip file
+            projectFile = ZipFile.Read(filename);
+
+            // Read project file inside of zip file
+            OpenProjectDirectoryFile(projectFile);
+
+            // Flag as open
+            projectOpen = true;
+        }
+
+        /// <summary>
+        /// Close an open project file.
+        /// </summary>
+        public void Close()
+        {
+            // Close document
+            if (projectDocument != null)
+            {
+                projectDocument = null;
+            }
+
+            // Close project file
+            if (projectFile != null)
+            {
+                projectFile.Dispose();
+                projectFile = null;
+            }
+
+            // Flag as closed
+            projectOpen = false;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Creates project directory inside specified project file.
+        /// </summary>
+        /// <param name="name">Name of directory file.</param>
+        /// <param name="file">Project zip file.</param>
+        private void CreateProjectDirectoryFile(string name, ZipFile file)
+        {
+            // Create document
+            projectDocument = XmlHelper.CreateXmlDocument(projectSchema);
+            XmlElement element = XmlHelper.AppendElement(projectDocument, null, projectElement, null);
+            element.SetAttribute(projectNameAttribute, name);
+
+            // Add project document to zip file
+            file.UpdateEntry(projectDirectoryFileName, XmlHelper.ToMemoryStream(projectDocument));
+        }
+
+        /// <summary>
+        /// Opens project directory inside specified project file.
+        /// </summary>
+        /// <param name="projectFile">Project zip file.</param>
+        private void OpenProjectDirectoryFile(ZipFile file)
+        {
+        }
+
+        /// <summary>
+        /// Updates project directory file inside project zip file.
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="document"></param>
+        private void UpdateProjectDirectoryFile(ZipFile file, XmlDocument document)
+        {
+        }
+
+        #endregion
+
     }
 
 }
